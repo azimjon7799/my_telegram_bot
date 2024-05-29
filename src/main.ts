b0861eeb7606fc8f5a6949191d221db3f959bfd9
Abduazim Usmon <azimjonusmonov602@gmail.com>
2024-05-29 17:39:45 +0500

    Some changes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CLIENT_RENEG_LIMIT } from 'tls';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+const start = async () => {
+  try {
+    const PORT = process.env.PORT || 3333;
+    const app = await NestFactory.create(AppModule);
+    app.setGlobalPrefix('api/v1');
+    
+    await app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
-bootstrap();
+start();
+
